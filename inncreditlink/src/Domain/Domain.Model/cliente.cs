bacd3717160ee19825de306164b9573b9c460691
Jhoanna Milena Zuluaga Bedoya <jzuluaga@sistecredito.com>
2023-06-27 22:03:46 -0500

    se agrega class cliente

---
+++ inncreditlink/src/Domain/Domain.Model/cliente.cs
@@ -0,0 +1,11 @@
+public class Cliente
+{
+    public int IdCliente { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Direccion { get; set; }
+    public string Telefono { get; set; }
+    public string Cupo { get; set; }
+    public string deuda { get; set; }
+
+}
